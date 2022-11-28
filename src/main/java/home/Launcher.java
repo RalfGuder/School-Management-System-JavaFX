e0b0a0c8d9abb2c0c0965b671217a5a122f1d7d2
RalfGuder <45797275+RalfGuder@users.noreply.github.com>
2022-11-28 10:21:59 +0100

    Patch 2: update fonts

---
+++ src/main/java/home/Launcher.java
@@ -0,0 +1,9 @@
+package home;
+
+public class Launcher {
+
+  public static void main(String[] args) {
+    Main.main(args);
+  }
+
+}
--- src/main/java/home/Main.java
+++ src/main/java/home/Main.java
@@ -6,15 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import jfxtras.styles.jmetro8.JMetro;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/Home.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Home.fxml"));
         primaryStage.setTitle("KeepToo SMSys");
-        primaryStage.getIcons().add(new Image("/home/icons/icon.png"));
+        primaryStage.getIcons().add(new Image("/icons/icon.png"));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
